4c9c5e6c6a4ff59133e2c213f0a483fa910e0d71
曹武斌 <caowubin@ehongcn.com>
2019-12-02 15:29:36 +0800

    feat: add recast.visit -- AST节点遍历;

---
+++ read.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+const recast = require("recast")
+
+recast.run(function (ast, printSource){
+	recast.visit(ast, {
+		visitExpressionStatement: function(path) {
+			const node = path.node
+			printSource(node)
+			this.traverse(path)
+		}
+	})
+})
+
